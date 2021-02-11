9113f715f9e35f5e07be28838916e2f181145e72
Tanvi Khot <orangetanvi@gmail.com>
2021-02-11 15:35:57 -0800

    Firebase Basic Authentication

--- LetsChat/Controllers/ConversationsViewController.swift
+++ LetsChat/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TestViewController.swift
 //  LetsChat
 //
 //  Created by Gaurav Khot on 2/6/21.
@@ -7,14 +7,23 @@
 
 import UIKit
 
-class
-ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .green
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
---
+++ LetsChat/Resources/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  LetsChat
+//
+//  Created by Gaurav Khot on 2/6/21.
+//
+
+import Foundation
